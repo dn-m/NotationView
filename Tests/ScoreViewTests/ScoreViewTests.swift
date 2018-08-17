96eaae894de5994377055e335846449017ab1923
James <jbean46@gmail.com>
2018-08-17 12:01:32 -0400

    Add stubs for ScoreView and ScoreViewTests

---
+++ Tests/ScoreViewTests/ScoreViewTests.swift
@@ -0,0 +1,16 @@
+//
+//  ScoreViewTests.swift
+//  ScoreViewTests
+//
+//  Created by James Bean on 8/17/17.
+//
+//
+
+import XCTest
+
+class ScoreViewTests: XCTestCase {
+
+    func testScoreView() {
+        #warning("TODO: Implement ScoreViewTests")
+    }
+}
