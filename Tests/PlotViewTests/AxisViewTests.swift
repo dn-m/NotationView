10e070ef7d4b49b0d3ef53aeae0918f3e3d8bba4
James <jbean46@gmail.com>
2018-08-17 11:05:53 -0400

    Silence inert AxisViewTests for linux main generation

--- Tests/PlotViewTests/AxisViewTests.swift
+++ Tests/PlotViewTests/AxisViewTests.swift
@@ -10,18 +10,19 @@ import XCTest
 import Rendering
 import PlotView
 
-class AxisViewTests: XCTestCase {
-    
-    struct ClefConfiguration { }
-    
-    struct ClefView: VerticalAxisView {
-        
-        /// `StyledPath.StyledPath.Composite`-representation of `Renderable`-conforming type.
-        var rendered: StyledPath.Composite {
-            fatalError()
-        }
-
-        let configuration: ClefConfiguration
-        let position: VerticalAxisPosition
-    }
-}
+#warning("Reinstate AxisViewTests")
+//class AxisViewTests: XCTestCase {
+//
+//    struct ClefConfiguration { }
+//
+//    struct ClefView: VerticalAxisView {
+//
+//        /// `StyledPath.StyledPath.Composite`-representation of `Renderable`-conforming type.
+//        var rendered: StyledPath.Composite {
+//            fatalError()
+//        }
+//
+//        let configuration: ClefConfiguration
+//        let position: VerticalAxisPosition
+//    }
+//}
