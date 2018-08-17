42b7c02587efe846e9b6aca939cbdbbefd06499f
James <jbean46@gmail.com>
2018-08-17 08:17:09 -0400

    Update Graphics to a release rather than master branch

--- Package.swift
+++ Package.swift
@@ -11,7 +11,7 @@ let package = Package(
         .library(name: "RhythmView", targets: ["RhythmView"])
     ],
     dependencies: [
-        .package(url: "https://github.com/dn-m/Graphics", .branch("master")),
+        .package(url: "https://github.com/dn-m/Graphics", from: "0.1.0"),
         .package(url: "https://github.com/dn-m/NotationModel", from: "0.3.0")
     ],
     targets: [
